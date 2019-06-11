a2417c081a83b4190ba3e67b971b335aeb5cc85c
Sabin Marcu <sabin.marcu@rga.com>
2019-06-11 15:47:50 +0300

    fix(logic): remove end date

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card, CardHeader, CardContent, TextField, Typography } from '@material-ui/core';
+import { DateFormatInput, TimeFormatInput } from 'material-ui-next-pickers';
+import styles from './App.module.css';
+import useEditor from './logic';
 
-function App() {
+const App = () => {
+  const {
+    output, 
+    title, setTitle, 
+    username, setUsername,
+    startTime, setStartTime, startDate, setStartDate,
+  } = useEditor();
+  const handlerMap = {
+    'title': setTitle, 
+    'username': setUsername,
+    'startDate': setStartDate,
+    'startTime': setStartTime,
+  };
+  const handleRawOnChange = prop => 
+    (value) =>
+      handlerMap[prop](value);
+  const handleOnChange = prop => 
+    ({ target: { value } }) => 
+      handleRawOnChange(prop)(value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Card className={styles.card}>
+        <CardHeader title="Generate Event" />
+        <CardContent className={styles.container}>
+          <div className={styles.editor}>
+            <TextField
+              fullWidth={true}
+              className={styles.input}
+              label={'Event Title'}
+              value={title}
+              onChange={handleOnChange('title')}
+            />
+            <TextField
+              fullWidth={true}
+              className={styles.input}
+              label={'Event Host'}
+              value={username}
+              onChange={handleOnChange('username')}
+            />
+            <div className={styles.dateField}>
+              <DateFormatInput 
+                className={styles.input}
+                label="Start Date"
+                {...(startDate ? {
+                  value: startDate,
+                } : {})}
+                onChange={handleRawOnChange('startDate')}
+                okToConfirm={true}
+              />
+              <TimeFormatInput 
+                className={styles.input}
+                label="Start Time"
+                {...(startTime ? {
+                  value: startTime,
+                } : {})}
+                onChange={handleRawOnChange('startTime')}
+                okToConfirm={true}
+              />
+            </div>
+          </div>
+          <div className={styles.preview}>
+            <Typography>{output ? 'Output' : 'Invalid Info'}</Typography>
+            {output && <TextField 
+              disabled
+              value={output}
+              multiline
+              fullWidth
+            />}
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
